1d6fd2faa1203e79ac1b22ad054128759b1dd66a
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-04 13:21:03 +0700

    penggunaan Row, Column, dan Padding

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,28 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text(widget.title)),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World',
-            ),
-          ],
-        ),
-      ),
+      body: Row(
+        children: [
+          Padding(padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(child: Text('Hello World 1', style: TextStyle(color: Colors.blue),),),
+              Center(child: Text('Hello World 1'),),
+              Center(child: Text('Hello World 1'),),
+            ],
+          ),),
+          Padding(padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(child: Text('Hello World 2', style: TextStyle(color: Colors.blue),),),
+              Center(child: Text('Hello World 2'),),
+              Center(child: Text('Hello World 2'),),
+            ],
+          ),)
+        ],
+      )
     );
   }
 }
