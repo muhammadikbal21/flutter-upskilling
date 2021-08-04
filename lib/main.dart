41ccda5bacb06a6e7344ab052f0e382263464b34
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-04 13:38:42 +0700

    memindahkan MyHomePage ke file baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_upskilling/my_home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,46 +14,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Row(
-        children: [
-          Padding(padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Center(child: Text('Hello World 1', style: TextStyle(color: Colors.blue),),),
-              Center(child: Text('Hello World 1'),),
-              Center(child: Text('Hello World 1'),),
-            ],
-          ),),
-          Padding(padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Center(child: Text('Hello World 2', style: TextStyle(color: Colors.blue),),),
-              Center(child: Text('Hello World 2'),),
-              Center(child: Text('Hello World 2'),),
-            ],
-          ),)
-        ],
-      )
     );
   }
 }
